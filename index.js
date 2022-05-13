c6794202367032d581a50e4b8220a0ba05ef72f2
gevorg-martir <gevorg.martir@gmail.com>
2022-05-13 13:44:31 +0400

    docs(readme): add ci status batch to readme

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const semanticRelease = require('semantic-release')
 const run = async () => {
   try {
     const publish = core.getInput('dryRun')
-    // const config = await fs.readFile('./.releaserc.yml', 'utf8')
-    // console.log(parse(config))
     const result = await semanticRelease({
       dryRun: !publish
     })
